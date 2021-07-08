d8303e22ea38022e2f158dfb733960ff57152c41
Casf16 <86844315+Casf16@users.noreply.github.com>
2021-07-08 11:54:27 +0800

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Creates a special matrix object that 
+## can cache the inverse of a matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+## value of the matrix
+  get <- function() {x}
+## set the value of the inverse
+  setInerse <- function(inverse) {inv <<- inverse}
+## get the value of the inverse
+  getInverse <- function() {inv}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  
 }
+## Double assignment operator is useful in conjunction. They modify variables in parent levels.
+#A function written by a function closest includes the parent function to have access to all the variables and parameters
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("currently getting cached data...wait...")
+    return(inv)
+  }
+  mat <- x$get()
+## to compute the inverse of a matrix solve is the standard R function
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
